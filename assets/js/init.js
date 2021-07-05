5c6fe3a9f61c2ca28de7233507ae3b464946a91a
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2021-07-05 17:19:23 +0700

    update sidebar

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const sideNavItems = document.querySelectorAll("nav.side-nav")
+  const sideNavMinify = document.querySelectorAll(".minify-sidenav")
   sideNavItems.forEach(function (e) {
     const items = e.querySelectorAll("a.has-child")
     items.forEach(function (e2) {
@@ -7,6 +8,16 @@ document.addEventListener('DOMContentLoaded', function () {
         e.preventDefault()
         if (e2.classList.contains("active")) e2.classList.remove("active")
         else e2.classList.add("active")
+      })
+    })
+  })
+
+  sideNavMinify.forEach(function (e) {
+    e.addEventListener("click", function (e) {
+      e.preventDefault()
+      sideNavItems.forEach(function (e2) {
+        if (e2.classList.contains("mini")) e2.classList.remove("mini")
+        else e2.classList.add("mini")
       })
     })
   })
