b555329d422d47d85db3f237aeb3727ea0431d3f
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2021-07-07 22:17:23 +0700

    support mobile

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,15 +1,17 @@
 const sideNavItems = document.querySelectorAll("nav.side-nav")
 const sideNavMinify = document.querySelectorAll(".minify-sidenav")
+const mediaQuery = window.matchMedia('(max-width: 768px)')
+const desktopQuery = window.matchMedia('(min-width: 769px)')
 document.addEventListener('DOMContentLoaded', function () {
   sideNavItems.forEach(function (e) {
     const items = e.querySelectorAll("a.has-child")
     items.forEach(function (e2) {
       e2.addEventListener("click", function (e3) {
         e3.preventDefault()
-        if (!e.classList.contains("mini")) {
-          if (e2.classList.contains("active")) e2.classList.remove("active")
-          else e2.classList.add("active")
-        }
+        // if (!e.classList.contains("mini")) {
+        if (e2.classList.contains("active")) e2.classList.remove("active")
+        else e2.classList.add("active")
+        // }
       })
     })
     sideNavMinifyButton(e)
@@ -38,13 +40,37 @@ document.addEventListener('DOMContentLoaded', function () {
   function changeButtonMinifyContent(state, e) {
     if (state === "mini") {
       e.innerHTML = `<i class="fa fa-align-justify"></i>`
-      sideNavItems.forEach(function (e) {
-        const items = e.querySelectorAll("a.has-child")
-        items.forEach(function (e2) {
-          e2.classList.remove("active")
-        })
-      })
     }
     else e.innerHTML = `<i class="fa fa-align-right"></i>`
   }
+
+  mediaQuery.addEventListener('change', function (e) {
+    if (e.matches) {
+      sideNavItems.forEach(function (e1) {
+        e1.classList.remove("mini")
+        e1.classList.add("hide")
+        reskaraDebounce(200, function () {
+          e1.setAttribute("style", "display: none")
+        })
+      })
+    }
+  })
+
+  desktopQuery.addEventListener('change', function (e) {
+    if (e.matches) {
+      sideNavItems.forEach(function (e1) {
+        e1.classList.remove("hide")
+        reskaraDebounce(200, function () {
+          e1.setAttribute("style", "display: block")
+        })
+      })
+    }
+  })
 })
+
+function reskaraDebounce(time = 0, callback = function () { }) {
+  var interval = setInterval(function () {
+    callback()
+    clearInterval(interval)
+  }, time)
+}
