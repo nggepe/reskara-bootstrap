2027081db463e67cadc6b9ec5642b1d2470d51b4
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2021-07-07 18:53:01 +0700

    minify

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,13 +1,15 @@
+const sideNavItems = document.querySelectorAll("nav.side-nav")
+const sideNavMinify = document.querySelectorAll(".minify-sidenav")
 document.addEventListener('DOMContentLoaded', function () {
-  const sideNavItems = document.querySelectorAll("nav.side-nav")
-  const sideNavMinify = document.querySelectorAll(".minify-sidenav")
   sideNavItems.forEach(function (e) {
     const items = e.querySelectorAll("a.has-child")
     items.forEach(function (e2) {
-      e2.addEventListener("click", function (e) {
-        e.preventDefault()
-        if (e2.classList.contains("active")) e2.classList.remove("active")
-        else e2.classList.add("active")
+      e2.addEventListener("click", function (e3) {
+        e3.preventDefault()
+        if (!e.classList.contains("mini")) {
+          if (e2.classList.contains("active")) e2.classList.remove("active")
+          else e2.classList.add("active")
+        }
       })
     })
     sideNavMinifyButton(e)
@@ -34,7 +36,15 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function changeButtonMinifyContent(state, e) {
-    if (state === "mini") e.innerHTML = `<i class="fa fa-align-justify"></i>`
+    if (state === "mini") {
+      e.innerHTML = `<i class="fa fa-align-justify"></i>`
+      sideNavItems.forEach(function (e) {
+        const items = e.querySelectorAll("a.has-child")
+        items.forEach(function (e2) {
+          e2.classList.remove("active")
+        })
+      })
+    }
     else e.innerHTML = `<i class="fa fa-align-right"></i>`
   }
 })
