0369154d1dafc0cfc13c778ebbdb153dd121a2bf
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2021-09-10 22:48:52 +0700

    update

--- assets/js/components.js
+++ assets/js/components.js
@@ -59,12 +59,13 @@ $(document).ready(function (e) {
   function validateHelper(formGroup, el) {
     if ($(el).val() == "") {
       if ($(formGroup).find(".text-helper").length == 0)
-        formGroup.append("<small class='text-danger text-helper'>Required field!</small>")
+        formGroup.append("<small class='text-danger text-helper'>Required field!</small>"),
+          formGroup.addClass("has-error")
     }
-    else formGroup.find(".text-helper").remove()
+    else formGroup.find(".text-helper").remove(), formGroup.removeClass("has-error")
 
     $(el).on("keyup", function (e) {
-      if ($(this).val() != "") formGroup.find(".text-helper").remove()
+      if ($(this).val() != "") formGroup.find(".text-helper").remove(), formGroup.removeClass("has-error")
     })
   }
 })
